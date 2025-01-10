dc5f30b76b4e42a4eb3266289e9a5b360a9600d6
glitch-Staticize <static.staticize@gmail.com>
2025-01-10 13:52:19 -0500

    Added some missing code because it didn't fully copy over for some reason

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,7 +1,35 @@
-import { defineStore } from 'pinia'
+import { ref } from "vue";
+import { defineStore } from "pinia";
 
 export const useStore = defineStore('store', () => {
-  // Add your state management code here
+  const cart = ref(new Map());
+  const currentUserEmail = ref('');
+  const accounts = ref(new Map());
 
-  return { }
-})
+  const setRegistrationData = (data) => {
+    const userAccount = {
+      firstName: data.firstName,
+      lastName: data.lastName,
+      email: data.email,
+      password: data.password,
+    };
+    accounts.value.set(data.email, userAccount);
+  };
+
+  function addToCart(id, movieData) {
+    cart.value.set(id, movieData);
+  }
+
+  function removeFromCart(id) {
+    cart.value.delete(id);
+  }
+
+  return {
+    cart,
+    currentUserEmail,
+    accounts,
+    addToCart,
+    removeFromCart,
+    setRegistrationData,
+  };
+});
